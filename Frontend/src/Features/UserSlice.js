a808cf29640b4ea9c2f38fb3976f624f75e52ad5
Abhishek Kumar <144116717+super-coder123@users.noreply.github.com>
2025-12-25 12:45:52 +0530

    Update API URL to production endpoint

--- Frontend/src/Features/UserSlice.js
+++ Frontend/src/Features/UserSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'; 
 import axios from 'axios'; 
 
-const API_URL = 'http://localhost:3000'; 
+const API_URL = 'https://personal-finance-tracker-backend-nx7k.onrender.com'; 
 
 
  const initialState = {
